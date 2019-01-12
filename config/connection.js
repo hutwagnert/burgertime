98582e7a3d3f0f0140c531979a0363878b61a2a0
hutwagner tate <hutwagnert@gmail.com>
2019-01-12 13:37:46 -0500

    new connection

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,8 @@
 var mysql = require("mysql");
 require('dotenv').config();
 var connection;
-var JAWSDB_url ='mysql://ben03awrpjvkcd4b:riwejfms6euiels3@tyduzbv3ggpf15sx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/xiv5brrf06ircwyw'
-if(JAWSDB_url){
-  connection = mysql.createConnection(JAWSDB_url);
+if(process.env.JAWSDB_url){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else{
 connection =  mysql.createConnection({
   host: process.env.DB_HOST,
